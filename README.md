703aa17427113f3e00eaf059eed26ad6ccff35e6
Michael Senescall <M.Senescall@hotmail.com>
2022-10-23 23:21:56 +1000

    Moved a few things around

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ An image to automatically update a [duckdns.org](https://www.duckdns.org/) DDNS.
  * **DOMAINS** - Hostname(s) to update (e.g., "example1.duckdns.org,example2.duckdns.org")
  * **TOKEN** - Duck DNS authentication token (e.g., "ix0wp0jk-3jwr-qtks-7gk4eg1rifm0")
  * **INTERVAL** - (Optional) Time between updates, default = "30m" (e.g., "30m" where excepted suffixes are "s", "m", "h", and "d" for second, minutes, hours, and days respectively)
- * **USE_IPV4** - (Optional) Use IPv4, default = "yes"
- * **USE_IPV6** - (Optional) Use IPv4, default = "no"
+ * **USE_IPV4** - (Optional) Use IPv4, default = "true"
+ * **USE_IPV6** - (Optional) Use IPv4, default = "false"
--- main.sh
+++ main.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Initialise some default variables
 INTERVAL=${INTERVAL:-"30m"}
-USE_IPV4=${USE_IPV4:-"yes"}
-USE_IPV6=${USE_IPV6:-"no"}
+USE_IPV4=${USE_IPV4:-"true"}
+USE_IPV6=${USE_IPV6:-"false"}
 
+# Make sure everything is lower case
 USE_IPV4=$(echo "${USE_IPV4}" | awk '{print tolower($0)}')
 USE_IPV6=$(echo "${USE_IPV6}" | awk '{print tolower($0)}')
+DOMAINS=$(echo "${DOMAINS}" | awk '{print tolower($0)}')
+TOKEN=$(echo "${TOKEN}" | awk '{print tolower($0)}')
 
 if [ -z "$DOMAINS" ]; then
 	echo "DOMAINS missing"
@@ -17,32 +21,26 @@ if [ -z "${TOKEN}" ]; then
 	exit 1
 fi
 
-if [ "${USE_IPV4}" != "yes" ] && [ "${USE_IPV6}" != "yes" ]; then
-	echo "At least one of USE_IPV4 and USE_IPV6 must be set to 'yes'"
+if [ "${USE_IPV4}" != "true" ] && [ "${USE_IPV6}" != "true" ]; then
+	echo "At least one of USE_IPV4 and USE_IPV6 must be set to 'true'"
 	exit 1
 fi
 
 while true; do
 	echo "Starting loop..."
 
-	# I put this here so everything works even if someone fiddles around in the terminal during operation
-	USE_IPV4=$(echo "${USE_IPV4}" | awk '{print tolower($0)}')
-	USE_IPV6=$(echo "${USE_IPV6}" | awk '{print tolower($0)}')
-	DOMAINS=$(echo "${DOMAINS}" | awk '{print tolower($0)}')
-	TOKEN=$(echo "${TOKEN}" | awk '{print tolower($0)}')
-
 	# Build base URL
 	URL="https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}"
 
 	# IPv4
-	if [ "${USE_IPV4}" = "yes" ]; then
+	if [ "${USE_IPV4}" = "true" ]; then
 		IPV4=$(curl -s -4 ifconfig.io)
 		echo "IPv4: ${IPV4}"
 		URL="${URL}&ip=${IPV4}"
 	fi
 
 	# IPv6
-	if [ "${USE_IPV6}" = "yes" ]; then
+	if [ "${USE_IPV6}" = "true" ]; then
 		IPV6=$(curl -s -6 ifconfig.io)
 		echo "IPv6: ${IPV6}"
 		URL="${URL}&ipv6=${IPV6}"
