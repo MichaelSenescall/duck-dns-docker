63105119b99da1d48dfc836a4ff6574f55457ff2
Michael Senescall <M.Senescall@hotmail.com>
2022-10-23 23:08:46 +1000

    Added some comments and changed a few things

--- main.sh
+++ main.sh
@@ -4,6 +4,9 @@ INTERVAL=${INTERVAL:-"30m"}
 USE_IPV4=${USE_IPV4:-"yes"}
 USE_IPV6=${USE_IPV6:-"no"}
 
+USE_IPV4=$(echo "${USE_IPV4}" | awk '{print tolower($0)}')
+USE_IPV6=$(echo "${USE_IPV6}" | awk '{print tolower($0)}')
+
 if [ -z "$DOMAINS" ]; then
 	echo "DOMAINS missing"
 	exit 1
@@ -14,7 +17,7 @@ if [ -z "${TOKEN}" ]; then
 	exit 1
 fi
 
-if [ "${USE_IPV4}" = "no" ] && [ "${USE_IPV6}" = "no" ]; then
+if [ "${USE_IPV4}" != "yes" ] && [ "${USE_IPV6}" != "yes" ]; then
 	echo "At least one of USE_IPV4 and USE_IPV6 must be set to 'yes'"
 	exit 1
 fi
@@ -22,10 +25,14 @@ fi
 while true; do
 	echo "Starting loop..."
 
-	URL="https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}"
-
+	# I put this here so everything works even if someone fiddles around in the terminal during operation
 	USE_IPV4=$(echo "${USE_IPV4}" | awk '{print tolower($0)}')
 	USE_IPV6=$(echo "${USE_IPV6}" | awk '{print tolower($0)}')
+	DOMAINS=$(echo "${DOMAINS}" | awk '{print tolower($0)}')
+	TOKEN=$(echo "${TOKEN}" | awk '{print tolower($0)}')
+
+	# Build base URL
+	URL="https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}"
 
 	# IPv4
 	if [ "${USE_IPV4}" = "yes" ]; then
@@ -41,6 +48,7 @@ while true; do
 		URL="${URL}&ipv6=${IPV6}"
 	fi
 	
+	# Call URL to update DDNS
 	echo "Calling URL: ${URL}"
 	RESPONSE=$(curl -s -k "${URL}" & wait)
 	echo "Duck DNS response: ${RESPONSE}"
