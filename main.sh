9b77f49457e0c53264c08f42faffbfd2e3b90d01
Michael Senescall <M.Senescall@hotmail.com>
2022-10-23 22:53:04 +1000

    Added IPv4 & IPv6 env vars

--- main.sh
+++ main.sh
@@ -1,4 +1,6 @@
 INTERVAL=${INTERVAL:-"30m"}
+USE_IPV4=${USE_IPV4:-"yes"}
+USE_IPV6=${USE_IPV6:-"no"}
 
 if [ -z "$DOMAINS" ]; then
 	echo "DOMAINS missing"
@@ -10,25 +12,27 @@ if [ -z "${TOKEN}" ]; then
 	exit 1
 fi
 
+if [ "${USE_IPV4}" = "no" ] && [ "${USE_IPV6}" = "no" ]; then
+	echo "At least one of USE_IPV4 and USE_IPV6 must be set to 'yes'"
+	exit 1
+fi
+
 while true; do
 	echo "Starting loop..."
 
 	URL="https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}"
 
-	IPV4=$(curl -4 ifconfig.io)
-	IPV6=$(curl -6 ifconfig.io)
-
-	if [ -z "${IPV4}" || "${IPV4}" = "curl: (7) Couldn't connect to server" ]; then
-		echo "No IPv4 address found."
-	else
-		echo "IPv4 Found: ${IPV4}"
+	# IPv4
+	if [ "${USE_IPV4,,}" = "yes" ]; then
+		IPV4=$(curl -4 ifconfig.io)
+		echo "IPv4: ${IPV4}"
 		URL="${URL}&ip=${IPV4}"
 	fi
 
-	if [ -z "${IPV6}" || "${IPV6}" = "curl: (7) Couldn't connect to server" ]; then
-		echo "No IPv6 address found."
-	else
-		echo "IPv6 Found: ${IPV6}"
+	# IPv6
+	if [ "${USE_IPV6,,}" = "yes" ]; then
+		IPV6=$(curl -6 ifconfig.io)
+		echo "IPv6: ${IPV6}"
 		URL="${URL}&ipv6=${IPV6}"
 	fi
 	
